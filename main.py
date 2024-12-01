fff0f16abf7157f152eeda66b1daae7fde5b9045
SeveralCups <thecuppo@protonmail.com>
2024-12-01 14:22:26 +0000

    Create CircleShape base class. Clean up formatting in main.py.

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ from constants import ASTEROID_MAX_RADIUS
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     print(
@@ -28,6 +32,9 @@ def main():
 
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+
+        # 60FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
